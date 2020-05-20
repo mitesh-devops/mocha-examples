b5a8ae8779909448e922b838b103afe14ee5f131
mitesh.gangaramani <mitesh.gangaramani@TeamCity>
2020-05-20 10:43:16 +0530

    TeamCity change in 'Mitesh-Test' project: 'kotlin' build configuration was removed

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -46,7 +46,6 @@ project {
     vcsRoot(HttpsGithubComMiteshDevopsIstanbulCodeCoverageExampleGitRefsHeadsMaster)
 
     buildType(Kuberetes)
-    buildType(Kotlin)
     buildType(BuildTest)
     buildType(ExpressApp)
     buildType(CodeCoverageExample)
@@ -286,19 +285,6 @@ object ExpressApp : BuildType({
                 gitlabApiUrl = "https://gitlab.com/api/v4"
                 accessToken = "credentialsJSON:c4ec1430-4f58-4ecc-b522-bc428f12ca5a"
             }
-        }
-    }
-})
-
-object Kotlin : BuildType({
-    name = "kotlin"
-
-    vcs {
-        root(DslContext.settingsRoot)
-    }
-
-    triggers {
-        vcs {
         }
     }
 })
